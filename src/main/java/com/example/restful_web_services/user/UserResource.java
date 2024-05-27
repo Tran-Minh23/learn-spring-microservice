cab8307da287c7358ff034d50628044e16a8d00c
DESKTOP-0FQEQD0\batha <bathackcf@gmail.com>
2024-05-27 23:14:40 +0700

    practice

--- src/main/java/com/example/restful_web_services/user/UserResource.java
+++ src/main/java/com/example/restful_web_services/user/UserResource.java
@@ -5,6 +5,8 @@ import java.util.List;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 @RestController
 public class UserResource {
@@ -23,4 +25,10 @@ public class UserResource {
     public User retrieveUser(@PathVariable int id) {
         return service.findOne(id);
     }
+
+    @PostMapping("users")
+    public void createUser(@RequestBody User user) {
+        service.save(user);
+    }
+
 }
