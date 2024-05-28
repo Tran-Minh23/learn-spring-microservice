df02a76c7c8e3228cdba023d70f41bf6fb4e5153
LOGIXTEK\minh.tran <minh.tran@logixtek.com>
2024-05-28 18:06:13 +0700

    add some more codes

--- src/main/java/com/example/restful_web_services/user/User.java
+++ src/main/java/com/example/restful_web_services/user/User.java
@@ -2,9 +2,16 @@ package com.example.restful_web_services.user;
 
 import java.time.LocalDate;
 
+import jakarta.validation.constraints.Past;
+import jakarta.validation.constraints.Size;
+
 public class User {
     private Integer id;
+
+    @Size(min = 2, message = "Name should have at least 2 chars")
     private String name;
+
+    @Past(message = "Birth date should be on the past")
     private LocalDate birthDate;
 
     public User(Integer id, String name, LocalDate birthDate) {
--- src/main/java/com/example/restful_web_services/user/UserDaoService.java
+++ src/main/java/com/example/restful_web_services/user/UserDaoService.java
@@ -30,6 +30,11 @@ public class UserDaoService {
 
     public User findOne(int id) {
         Predicate<? super User> predicate = user -> user.getId().equals(id);
-        return users.stream().filter(predicate).findFirst().orElseThrow();
+        return users.stream().filter(predicate).findFirst().orElse(null);
+    }
+
+    public void deleteById(int id) {
+        Predicate<? super User> predicate = user -> user.getId().equals(id);
+        users.removeIf(predicate);
     }
 }
