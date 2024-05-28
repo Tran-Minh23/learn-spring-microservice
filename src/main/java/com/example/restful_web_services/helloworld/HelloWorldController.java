5597c8b3b76139cd3bdee97ac7d396c640054f53
DESKTOP-0FQEQD0\batha <bathackcf@gmail.com>
2024-05-28 23:04:47 +0700

    add some pratices

--- src/main/java/com/example/restful_web_services/helloworld/HelloWorldController.java
+++ src/main/java/com/example/restful_web_services/helloworld/HelloWorldController.java
@@ -1,11 +1,22 @@
 package com.example.restful_web_services.helloworld;
 
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Locale;
+
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 @RestController
 public class HelloWorldController {
+
+    private MessageSource messageSource;
+
+    public HelloWorldController(MessageSource messageSource) {
+        this.messageSource = messageSource;
+    }
 
     @GetMapping("hello-world")
     public String helloWorld() {
@@ -20,5 +31,11 @@ public class HelloWorldController {
     @GetMapping(path = "/hello-world/{name}")
     public HelloWorldBean helloWorldPath(@PathVariable String name) {
         return new HelloWorldBean("Hello cc " + name);
+    }
+
+    @GetMapping("hello-world-internationalized")
+    public String helloWorldInternationalized() {
+        Locale locale = LocaleContextHolder.getLocale();
+        return messageSource.getMessage("good.morning.message", null, "Default message", locale);
     }
 }
