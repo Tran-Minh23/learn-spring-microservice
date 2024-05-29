7fd479632b68133717093c55c32580e1a09ede10
LOGIXTEK\minh.tran <minh.tran@logixtek.com>
2024-05-29 17:51:32 +0700

    add practices

---
+++ src/main/resources/data.sql
@@ -0,0 +1,17 @@
+insert into user_details(id,birth_date,name)
+values(10001, current_date(), 'Mink1');
+
+insert into user_details(id,birth_date,name)
+values(10002, current_date(), 'Mink2');
+
+insert into user_details(id,birth_date,name)
+values(10003, current_date(), 'Mink3');
+
+insert into post(id,description,user_id)
+values(20001, 'I want to learn AWS', 10001);
+
+insert into post(id,description,user_id)
+values(20002, 'I want to learn Google', 10001);
+
+insert into post(id,description,user_id)
+values(20003, 'I want to learn CC', 10002);
