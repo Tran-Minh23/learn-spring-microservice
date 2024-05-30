0609916e7622c0aed3fd0100ff42e55404ff6502
LOGIXTEK\minh.tran <minh.tran@logixtek.com>
2024-05-30 14:57:21 +0700

    remove unused code

--- src/main/java/com/example/restful_web_services/filtering/FilteringController.java
+++ src/main/java/com/example/restful_web_services/filtering/FilteringController.java
@@ -8,7 +8,6 @@ import com.fasterxml.jackson.databind.ser.impl.SimpleFilterProvider;
 
 import org.springframework.http.converter.json.MappingJacksonValue;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 @RestController
 public class FilteringController {
